18583fd03987e99cae0ebbbcb9fa6ba6b0bf2d33
Steffen Kampmann <steffen.kampmann@gmail.com>
2015-03-29 11:02:49 +0200

    Reformatted file to tabs

--- MC_SensorPython/MC_SensorPython.py
+++ MC_SensorPython/MC_SensorPython.py
@@ -1,7 +1,7 @@
 #!/user/bin/python
 
 '''
-    based on http://blog.kloud.com.au/2014/10/11/the-internet-of-things-with-arduino-azure-event-hubs-and-the-azure-python-sdk/
+	based on http://blog.kloud.com.au/2014/10/11/the-internet-of-things-with-arduino-azure-event-hubs-and-the-azure-python-sdk/
 '''
 
 import sys
@@ -15,59 +15,62 @@ from azure.servicebus.servicebusservice import ServiceBusService, ServiceBusSASA
 from azure.http import HTTPRequest, HTTPError
 from azure.http.httpclient import _HTTPClient
 
+
 class EventHubClient(object):
 
-  def __init__(self, service_bus_namespace, event_hub_name, policy_name, policy_key):
-    self.hostname = socket.gethostname()
-    self.service_bus_namespace, self.event_hub_name = service_bus_namespace, event_hub_name
-    self.policy_name, self.policy_key = policy_name, policy_key
+	def __init__(self, service_bus_namespace, event_hub_name, policy_name, policy_key):
+		self.hostname = socket.gethostname()
+		self.service_bus_namespace, self.event_hub_name = service_bus_namespace, event_hub_name
+		self.policy_name, self.policy_key = policy_name, policy_key
 
-  def send_message(self, body, partition):
-    httpclient = _HTTPClient(service_instance=self)
+	def send_message(self, body, partition):
+		httpclient = _HTTPClient(service_instance=self)
 
-    # build request
-    request = HTTPRequest()
-    request.method = 'POST'
-    request.host = '%s.servicebus.windows.net' % self.service_bus_namespace
-    request.protocol_override = 'https'
-    request.path = '/%s/publishers/%s/messages?api-version=2014-05' % (self.event_hub_name, partition)
-    request.body = body
-    request.headers.append(('Content-Type', 'application/atom+xml;type=entry;charset=utf-8'))
-    
-    # authenticate request
-    authentication = ServiceBusSASAuthentication(self.policy_name, self.policy_key)
-    authentication.sign_request(request, httpclient)
+		# build request
+		request = HTTPRequest()
+		request.method = 'POST'
+		request.host = '%s.servicebus.windows.net' % self.service_bus_namespace
+		request.protocol_override = 'https'
+		request.path = '/%s/publishers/%s/messages?api-version=2014-05' % (self.event_hub_name, partition)
+		request.body = body
+		request.headers.append(('Content-Type', 'application/atom+xml;type=entry;charset=utf-8'))
+	
+		# authenticate request
+		authentication = ServiceBusSASAuthentication(self.policy_name, self.policy_key)
+		authentication.sign_request(request, httpclient)
 
-    # verify request
-    request.headers.append(('Content-Length', str(len(request.body))))
+		# verify request
+		request.headers.append(('Content-Length', str(len(request.body))))
 
-    # send request
-    status = 0
+		# send request
+		status = 0
 
-    try:
-        resp = httpclient.perform_request(request)
-        status = resp.status
+		try:
+			resp = httpclient.perform_request(request)
+			status = resp.status
 
-    except HTTPError as ex:
-        status = ex.status
+		except HTTPError as ex:
+			status = ex.status
 
-    return status
+		return status
 
-  def send_measurement(self, payload, sensor_id):
-    return self.send_message(json.dumps(payload), sensor_id)
+	def send_measurement(self, payload, sensor_id):
+		return self.send_message(json.dumps(payload), sensor_id)
+
 
 class Telementry:
 
-    def __init__(self, device_id):
-        self.device_id = device_id
+	def __init__(self, device_id):
+		self.device_id = device_id
 
-    def measure(self):
-        return {
-            'DeviceId': self.device_id,
-            'Temperature': 27.5,
-            'Humidity': 68.3,
-            'Pollution': random.random() * 100
-        }
+	def measure(self):
+		return {
+			'DeviceId': self.device_id,
+			'Temperature': 27.5,
+			'Humidity': 68.3,
+			'Pollution': random.random() * 100
+		}
+
 
 hubClient = EventHubClient('myservicebusnamespace', 'myhub', 'SendPolicy', 'erENqf/5wdWCNEbCA9NsDIRqd5MRKdkii07+wezl/NU=')
 sensor = Telementry('Device-50')
